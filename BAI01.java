ec171f7aa65b9668630f3ac6a8c839def0f321c9
LeTranTanPhat <1250080137@sv.hcmunre.edu.vn>
2025-03-31 14:31:37 +0700

    Add files via upload

--- BAI01.java
+++ BAI01.java
@@ -1,10 +1,20 @@
 package BAITAP_LAB01;
+import java.util.Scanner;
 
 public class BAI01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String hoten;
+		String ngaysinh;
+		String mssv;
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Nhap ten: ");
+		hoten = kb.nextLine();
+		System.out.println("Nhap ngay thang nam sinh: ");
+		ngaysinh = kb.nextLine();
+		System.out.println("Nhap ma so sinh vien: ");
+		mssv = kb.nextLine();
 	}
 
 }
